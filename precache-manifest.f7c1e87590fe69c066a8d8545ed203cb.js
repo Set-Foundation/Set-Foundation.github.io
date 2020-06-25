6edd69658edd96f1eee60d122614b013d07f2037
Victor Wang <victorwang2001@gmail.com>
2020-06-24 21:16:41 -0400

    seo and more logos

---
+++ precache-manifest.f7c1e87590fe69c066a8d8545ed203cb.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11f64fbb9fdded95372b53fd7eb8804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52f358b1f0043eed5b6b",
+    "url": "/static/css/2.318dae63.chunk.css"
+  },
+  {
+    "revision": "c3fd48fef76470f931fb",
+    "url": "/static/css/main.eb06ac42.chunk.css"
+  },
+  {
+    "revision": "52f358b1f0043eed5b6b",
+    "url": "/static/js/2.bb179550.chunk.js"
+  },
+  {
+    "revision": "00ba9122ace4f7cfb2b78b0b307611b4",
+    "url": "/static/js/2.bb179550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3fd48fef76470f931fb",
+    "url": "/static/js/main.858530d8.chunk.js"
+  },
+  {
+    "revision": "365c827449d68ea19b66",
+    "url": "/static/js/runtime-main.6c8b38f3.js"
+  },
+  {
+    "revision": "5005556de4cc2ee39c35e7e306747fb5",
+    "url": "/static/media/01.5005556d.png"
+  },
+  {
+    "revision": "b611b420f8494b7881117b81ea12935a",
+    "url": "/static/media/02.b611b420.png"
+  },
+  {
+    "revision": "68858b4076ed0c2e960268afbe606731",
+    "url": "/static/media/03.68858b40.png"
+  },
+  {
+    "revision": "a60699c367a75f56f344e31f7745987f",
+    "url": "/static/media/04.a60699c3.png"
+  },
+  {
+    "revision": "59895d7d67b54dfd6307a4cc83e82067",
+    "url": "/static/media/05.59895d7d.png"
+  },
+  {
+    "revision": "80348ce181f7d941cf214a39ff36f9d2",
+    "url": "/static/media/06.80348ce1.png"
+  },
+  {
+    "revision": "e9eedd4f4b5f255d06d49e0a521a1a6b",
+    "url": "/static/media/07.e9eedd4f.png"
+  },
+  {
+    "revision": "c603f444e873facd4ec3687366c350a7",
+    "url": "/static/media/08.c603f444.png"
+  },
+  {
+    "revision": "6098fe64f553a6f2b43fa6b5e4c34b74",
+    "url": "/static/media/09.6098fe64.png"
+  },
+  {
+    "revision": "38619f69acdabbe2c74f6206a9e93871",
+    "url": "/static/media/10.38619f69.png"
+  },
+  {
+    "revision": "844c065766b2da4b4e030f49edd711dd",
+    "url": "/static/media/11.844c0657.png"
+  },
+  {
+    "revision": "2f361d6d6c656f056c270e6517fc489e",
+    "url": "/static/media/12.2f361d6d.png"
+  },
+  {
+    "revision": "95f51b46ab892a92677e82a26378bfc9",
+    "url": "/static/media/13.95f51b46.png"
+  },
+  {
+    "revision": "fc153b9213cbc907ab9d5ae19bc10848",
+    "url": "/static/media/14.fc153b92.png"
+  },
+  {
+    "revision": "376331ae114f8d1cbfb20bf5c752aa6c",
+    "url": "/static/media/15.376331ae.png"
+  },
+  {
+    "revision": "4e9a60e0a9ce80795bf372c8a9668df8",
+    "url": "/static/media/16.4e9a60e0.png"
+  },
+  {
+    "revision": "81f4a8c47202c46ba41bf728a3533c52",
+    "url": "/static/media/17.81f4a8c4.png"
+  },
+  {
+    "revision": "88e3daf58c859967fd49f7a7f3e8711c",
+    "url": "/static/media/1P-com-wordmark-536px-tall.88e3daf5.png"
+  },
+  {
+    "revision": "21a30078414ab26066da59b970da23f4",
+    "url": "/static/media/Bugsee-Logo.21a30078.svg"
+  },
+  {
+    "revision": "8b7d05b830144311e89a1c6b3505736d",
+    "url": "/static/media/Hackathon_-_Case_Package-4.8b7d05b8.pdf"
+  },
+  {
+    "revision": "71f206e315e460b88cdeb8ad80f9d998",
+    "url": "/static/media/Hackathon_logo_dev_1.71f206e3.png"
+  },
+  {
+    "revision": "a9da4350b4f191c5706dcac134465c21",
+    "url": "/static/media/Sponsorship_Package.a9da4350.pdf"
+  },
+  {
+    "revision": "8e305768da82da545989596edd6b2b93",
+    "url": "/static/media/balsamiq-logo-noborder-print.8e305768.svg"
+  },
+  {
+    "revision": "5ed81d50c93529c5aa489a9b45bc02b7",
+    "url": "/static/media/burger.5ed81d50.svg"
+  },
+  {
+    "revision": "60e742aedd833b4c8d91376ae0547b11",
+    "url": "/static/media/devpost-full.60e742ae.svg"
+  },
+  {
+    "revision": "4692c570437a9391826c31faebb6a504",
+    "url": "/static/media/echoAR.4692c570.png"
+  },
+  {
+    "revision": "06468580303d77d9afd2801a2ca80086",
+    "url": "/static/media/facebook_logo.06468580.svg"
+  },
+  {
+    "revision": "ea056d93940037b0d814a82d8cdb098f",
+    "url": "/static/media/instagram_logo.ea056d93.svg"
+  },
+  {
+    "revision": "c51eaef4af3dc9c047bf5a9e04ea8d57",
+    "url": "/static/media/linode-logo.c51eaef4.png"
+  },
+  {
+    "revision": "8e0bb13b5d81bf0f7f59aa99f63f2850",
+    "url": "/static/media/replit.8e0bb13b.png"
+  },
+  {
+    "revision": "af31655d1a058ce1ad13bf17c352bbe3",
+    "url": "/static/media/tohacks.af31655d.png"
+  },
+  {
+    "revision": "9da45a52d59c44ddd389677dcef820b0",
+    "url": "/static/media/wolfram-language-logo.9da45a52.png"
+  }
+]);
